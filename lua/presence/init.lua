01239e1af3717d2c42978f04b854ef3897a855d4
Andres Puello <77623794+afrolino02@users.noreply.github.com>
2024-12-22 21:04:25 -0500

    Update init.lua

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -135,16 +135,17 @@ function Presence:setup(...)
     -- File assets options
     self:set_option("file_assets", {})
     -- Asegúrate de definir tus opciones iniciales si no están definidas
-self.options.file_assets = self.options.file_assets or {}
+
 
 for name, asset in pairs(default_file_assets) do
     if not self.options.file_assets[name] then
         self.options.file_assets[name] = asset
     end
+        self.options.file_assets[".component.ts"] = { "Angular Component", "https://i.ibb.co/jDvq1mV/angular-svgrepo-com.png" }
 end
 
 -- Añadir específicamente el .component.ts después del bucle
-self.options.file_assets[".component.ts"] = { "Angular Component", "https://i.ibb.co/jDvq1mV/angular-svgrepo-com.png" }
+
 
 
 -- Asegurarse de que `Presence.get_file_extension` maneje correctamente las extensiones .component.ts
